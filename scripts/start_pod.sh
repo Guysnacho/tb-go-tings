72ba17156434b1e61b25426018c42e3e97f98536
Guysnacho <tunjiprod@gmail.com>
2026-08-23 20:13:01 -0500

    meh

--- scripts/start_pod.sh
+++ scripts/start_pod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-podman build -t kafka-broker -f Dockerfile.kafka .
-podman build -t tigerbeetle -f Dockerfile.tigerbeetle .
+podman build -t kafka-broker -f Dockerfile.kafka ../infra/.
+podman build -t tigerbeetle -f Dockerfile.tigerbeetle ../infra/.
 podman run -d --name broker --hostname broker -p 9092:9092 --replace -p 29092:29092 kafka-broker
 podman run -d --name tigerbeetle --hostname tigerbeetle --replace -p 3000:3000 --security-opt seccomp=unconfined tigerbeetle
